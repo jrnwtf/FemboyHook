70c24d6862939922c45f51430878699cbeb65861
jjj <jrnw1706@gmail.com>
2025-06-06 16:54:17 +0200

    Create CClientState_GetTime.cpp

---
+++ Amalgam/src/Hooks/CClientState_GetTime.cpp
@@ -0,0 +1,20 @@
+#include "../SDK/SDK.h"
+
+MAKE_SIGNATURE(CClientState_GetTime, "engine.dll", "80 B9 ? ? ? ? ? 66 0F 6E 81", 0x0);
+MAKE_SIGNATURE(CL_FireEvents_CClientState_GetTime_Call, "engine.dll", "E8 ? ? ? ? 0F 2F 45", 0x5);
+
+MAKE_HOOK(CClientState_GetTime, S::CClientState_GetTime(), float, void* rcx)
+{
+#ifdef DEBUG_HOOKS
+	if (!Vars::Hooks::CClientState_GetTime[DEFAULT_BIND])
+		return CALL_ORIGINAL(rcx);
+#endif
+
+	static auto dwDesired = S::CL_FireEvents_CClientState_GetTime_Call();
+	const auto dwRetAddr = std::uintptr_t(_ReturnAddress());
+
+	if (dwRetAddr == dwDesired)
+		return std::numeric_limits<float>::max();
+
+	return CALL_ORIGINAL(rcx);
+}
